80fe0a1782d1e8c3635d14a87b43fa641163b13c
dorinekiplagat90 <dorinekiplagat90@gmail.com>
2025-09-18 13:02:15 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+// Your code goes heredocument.addEventListener('DOMContentLoaded', () => {
+  
+document.addEventListener('DOMContentLoaded', () => {
+  const paragraph = document.querySelector('p');
+  paragraph.textContent = 'This is really cool!';
+});
